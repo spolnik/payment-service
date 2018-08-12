f278c1cf65459b4cd2cc9c70a3bad7bb944e909f
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-12 21:43:34 +0200

    - add custom Money serializer/deserializer for gson to simplify JSON schema

--- src/main/java/io/payments/api/Common.java
+++ src/main/java/io/payments/api/Common.java
@@ -1,12 +1,43 @@
 package io.payments.api;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+
+import java.lang.reflect.Type;
 
 public class Common {
-    private static final Gson gson = new Gson();
+    private static final Gson gson;
+
+    static {
+        gson = new GsonBuilder().
+                registerTypeAdapter(Money.class, new MoneyTransformer()).
+                create();
+    }
 
     public static Gson gson() { return gson; }
     public static String json() {
         return "application/json";
+    }
+
+    private static class MoneyTransformer implements JsonSerializer<Money>, JsonDeserializer<Money> {
+        @Override
+        public Money deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            JsonObject jsonObject = json.getAsJsonObject();
+            return Money.of(
+                    CurrencyUnit.getInstance(jsonObject.get("currency").getAsString()),
+                    jsonObject.get("amount").getAsBigDecimal()
+            );
+        }
+
+        @Override
+        public JsonElement serialize(Money src, Type typeOfSrc, JsonSerializationContext context) {
+            JsonObject jsonObject = new JsonObject();
+
+            jsonObject.addProperty("currency", src.getCurrencyUnit().getCode());
+            jsonObject.addProperty("amount", src.getAmount());
+
+            return jsonObject;
+        }
     }
 }
