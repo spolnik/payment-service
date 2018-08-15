26fcf9b78782cc23f4a80a3e0a066a2c8600c27c
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-15 17:35:03 +0200

    - further refactoring/simplification

--- src/main/java/io/payments/api/PaymentsRouter.java
+++ src/main/java/io/payments/api/PaymentsRouter.java
@@ -4,14 +4,12 @@ import spark.RouteGroup;
 import spark.Spark;
 
 import javax.inject.Inject;
-import java.util.List;
 import java.util.Set;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 public class PaymentsRouter implements Router {
 
     private static final String API_PREFIX = "/api";
+
     private final Set<VersionedApi> api;
 
     @Inject
@@ -21,17 +19,11 @@ public class PaymentsRouter implements Router {
 
     @Override
     public RouteGroup routes() {
-        return () -> {
-            List<String> versions = api.stream()
-                    .map(VersionedApi::version).distinct().collect(Collectors.toList());
-
-            versions.forEach(versionedPath());
-        };
-    }
-
-    private Consumer<String> versionedPath() {
-        return version ->
-                Spark.path("/" + version, versionedApi(version));
+        return () -> api.stream()
+                .map(VersionedApi::version)
+                .distinct()
+                .forEach(version ->
+                        Spark.path("/" + version, versionedApi(version)));
     }
 
     private RouteGroup versionedApi(String version) {
